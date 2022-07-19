722e03223aaee84a0308aff52622bdf9a971eb7d
Pedro <pwbarbosa@gmail.com>
2022-07-19 15:02:43 -0400

    record label and song pojos complete commit

--- backend-api/src/main/java/org/wcci/apimastery/Model/RecordLabel.java
+++ backend-api/src/main/java/org/wcci/apimastery/Model/RecordLabel.java
@@ -1,16 +1,54 @@
 package org.wcci.apimastery.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 public class RecordLabel {
     @Id
     @GeneratedValue
     private Long id;
+    private String name;
+    @OneToMany
+    @JsonIgnore
+    private Collection<Album> albums;
 
+    public RecordLabel(String name, Collection<Album> albums) {
+        this.name = name;
+        this.albums = albums;
+    }
 
     public RecordLabel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Collection<Album> getAlbums() {
+        return albums;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RecordLabel)) return false;
+        RecordLabel that = (RecordLabel) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
     }
 }
--- backend-api/src/main/java/org/wcci/apimastery/Model/Song.java
+++ backend-api/src/main/java/org/wcci/apimastery/Model/Song.java
@@ -3,14 +3,67 @@ package org.wcci.apimastery.Model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 public class Song {
     @Id
     @GeneratedValue
     private Long id;
+    private String title;
+    private String linkUrl;
+    private String duration;
+    private double avgRating;
+    @OneToMany
+    private Collection<Comment> comments;
 
+    public Song(String title, String linkUrl, String duration, double avgRating, Collection<Comment> comments) {
+        this.title = title;
+        this.linkUrl = linkUrl;
+        this.duration = duration;
+        this.avgRating = avgRating;
+        this.comments = comments;
+    }
 
     public Song() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLinkUrl() {
+        return linkUrl;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public double getAvgRating() {
+        return avgRating;
+    }
+
+    public Collection<Comment> getComments() {
+        return comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Song)) return false;
+        Song song = (Song) o;
+        return Double.compare(song.getAvgRating(), getAvgRating()) == 0 && Objects.equals(getId(), song.getId()) && Objects.equals(getTitle(), song.getTitle()) && Objects.equals(getLinkUrl(), song.getLinkUrl()) && Objects.equals(getDuration(), song.getDuration());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getLinkUrl(), getDuration(), getAvgRating());
     }
 }
