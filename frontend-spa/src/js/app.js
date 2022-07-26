773a636b2fa14b8ba603ee2b45321555aacebe6b
anathans1 <alexnathans1993@gmail.com>
2022-07-26 13:41:38 -0400

    add album popup added

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -19,19 +19,29 @@ const container = document.querySelector(".container");
 const changeAlbumNameButton = document.querySelector(".change-album-name-button");
 const changeAlbumNameText = container.querySelector("#popup");
 console.log(changeAlbumNameButton)
-changeAlbumNameButton.addEventListener("click", () =>{
+changeAlbumNameButton.addEventListener("click", () => {
   changeAlbumNameText.classList.toggle("hide");
   changeAlbumNameText.classList.toggle("show");
 })
 
 function makeHomeView() {
-  // fetch(LOCALHOST MAPPING HERE!!!!!!!!)
-  //   .then(res => res.json())
-  //   .then(albums => {
-      makeHomeViewFromJson({})
-    // })
+  fetch("http://localhost:8080/api/albums")
+    .then(res => res.json())
+    .then(albums => {
+      makeHomeViewFromJson(albums)
+    })
 }
-
+let popup = document.getElementById("popup");
+function showSocials() {
+  popup.classList.add("open-popup");
+}
+function hideSocials() {
+  popup.classList.remove("open-popup");
+}
+const addAlbumButton = container.querySelector("#addAlbumButton")
+addAlbumButton.addEventListener("click", () => {
+  showSocials();
+})
 function makeHomeViewFromJson(albums) {
   console.log(albums);
   container.innerHTML += header();
@@ -50,8 +60,36 @@ function makeHomeViewFromJson(albums) {
       })
     })
 
+    const albumTitleIn = container.querySelector("#album-title");
+    const artistNameIn = container.querySelector("#artist-name");
+    const albumImageURLIn = container.querySelector("#album-image-url");
+    const recordLabelIn = container.querySelector("#record-label");
+
+    const submitAlbumButton = container.querySelector("#submitAlbumButton");
+    submitAlbumButton.addEventListener("click", () => {
+      showSocials();
+      const newAlbumJson = {
+        "title": albumTitleIn.value,
+        "artistName": artistNameIn.value,
+        "linkURL": albumImageURLIn.value,
+        "recordLabel": recordLabelIn.value,
+      }
+
+      fetch(`http://localhost:8080/api/albums/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newAlbumJson)
+      })
+        .then(res => res.json())
+        .then(album => {
+          makeSongListView(album)
+        })
+    })
 
   })
+
 }
 
 function makeSongListView(songs) {
@@ -70,63 +108,66 @@ function makeSongListView(songs) {
       let recordLabelEl = album.querySelector("#record-label");
       let songNameEl = album.querySelector(".box-item");
       let playArrowEl = album.querySelector(".play-arrow")
-      songElement.forEach(song=> {
-        playArrowEl.addEventListener("click", () =>{
+      songElement.forEach(song => {
+        playArrowEl.addEventListener("click", () => {
           let singleSong = song.querySelector(".play-arrow");
           makeSingleSongView(songJson);
-      })
-      
-        
+        })
+
+
       })
     })
   })
-const backbutton = container.querySelector("#backbutton");
-backbutton.addEventListener("click", () =>{
-  makeHomeView();
-})
-
-const changeAlbumNameButton = container.querySelector(".change-album-name-button");
-const changeAlbumNameText = container.querySelector("#popup");
-console.log(changeAlbumNameText)
-changeAlbumNameButton.addEventListener("click", () =>{
-  changeAlbumNameText.classList.toggle("hide");
-  changeAlbumNameText.classList.toggle("show");
-})
-
-const deleteAlbumButton = container.querySelector(".delete-album-button");
-deleteAlbumButton.addEventListener("click", () => {
-  fetch(`HTTP MAPPING FOR DELETE${albumElId.value}`,{
-    method: 'DELETE'
+  const backbutton = container.querySelector("#backbutton");
+  backbutton.addEventListener("click", () => {
+    makeHomeView();
   })
-  .then(res => res.json())
-  .then(newAlbums => {
-makeHomeViewFromJson(newAlbums);
-//TODO review this code
+
+  const changeAlbumNameButton = container.querySelector(".change-album-name-button");
+  const changeAlbumNameText = container.querySelector("#popup");
+  console.log(changeAlbumNameText)
+  changeAlbumNameButton.addEventListener("click", () => {
+    changeAlbumNameText.classList.toggle("hide");
+    changeAlbumNameText.classList.toggle("show");
   })
-})
 
-const titleIn = container.querySelector(".titleNameInput");
-const linkURLIn = container.querySelector(".linkURLInput");
-const durationIn = container.querySelector(".durationInput");
-const albumIn = container.querySelector(".albumInput");
-const addSongToAlbumButton = songs.querySelector(".add-song-to-album-button");
-addSongToAlbumButton.addEventListener("click", () => {
-  const newSongJson = {
-    "title": titleIn.value,
-    "linkURL": linkURLIn.value,
-    "duration": durationIn.value,
-    "album": albumIn.value,
-  }
-  // fetch(`HTTP MAPPING FOR ADD/${song.id}/addSong`,
-  // method: "POST"
-  // )
-})
+  const deleteAlbumButton = container.querySelector(".delete-album-button");
+  deleteAlbumButton.addEventListener("click", () => {
+    fetch(`HTTP MAPPING FOR DELETE${albumElId.value}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(newAlbums => {
+        makeHomeViewFromJson(newAlbums);
+        //TODO review this code
+      })
+  })
 
-changeSongNameButton = songs.querySelector(".change-song-name-button");
+  const titleIn = container.querySelector(".titleNameInput");
+  const linkURLIn = container.querySelector(".linkURLInput");
+  const durationIn = container.querySelector(".durationInput");
+  const albumIn = container.querySelector(".albumInput");
+  const addSongToAlbumButton = songs.querySelector(".add-song-to-album-button");
+  addSongToAlbumButton.addEventListener("click", () => {
+    const newSongJson = {
+      "title": titleIn.value,
+      "linkURL": linkURLIn.value,
+      "duration": durationIn.value,
+      "album": albumIn.value,
+    }
+    // fetch(`HTTP MAPPING FOR ADD/${song.id}/addSong`,
+    // method: "POST"
+    // )
+  })
+
+  changeSongNameButton = songs.querySelector(".change-song-name-button");
   const deleteSongButton = songs.querySelector(".delete-song-button");
   deleteSongButton.addEventListener
-  
+
 }
+
+
+
 
 
 // makeSingleSongView(song) {
--- frontend-spa/src/js/home.js
+++ frontend-spa/src/js/home.js
@@ -12,10 +12,15 @@ export default function home() {
                 <canvas id="canvas"></canvas>
                 <img id="touchtunes" src="/src/images/BLANK-TOUCHTUNES.png" alt="">
                 <canvas id="canvas"></canvas>
-                <a id="backbutton" href="#"><img id="backbuttonimg" src="/src/images/backbutton.png" alt=""></a>
+                <a id="backbutton" href="#"><img id="backbuttonimg" src="/src/images/backbutton.png" alt="Neon Back Button"></a>
+                
                 <section class="projects">
                     <div class="slider">
-            <span class="album" style="--i:2;"><img class="album-cover" src="/src/images/ALLTHERIGHTREASONS.jpg"
+                    <span id="addAlbumButton" class="album" style="--i:1;"><img class="album-cover" src="/src/images/add-album-button.png"
+                            onClick="document.getElementById('COF').scrollIntoView();">
+                        </a>
+                    </span>
+            <span class="album" style="--i:${};"><img class="album-cover" src="/src/images/ALLTHERIGHTREASONS.jpg"
                     onClick="document.getElementById('ALLTHERIGHTREASONS').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});">
                 </a>
                 <p class="artist-name-home">Nickleback</p>
@@ -24,7 +29,31 @@ export default function home() {
            
                     </div>
                 </section>
-            </div>
+                <div class="popup" id="popup">
+                    
+                    <h2>Add Album</h2>
+                    <input id="album-title" class="rating-comment-submit" type="text"
+                        placeholder="Title...">
+                    <br>
+                    <br>
+                    <input id="artist-name" class="rating-comment-submit" type="text" 
+                        placeholder="Artist Name">
+                    <br>
+                    <br>
+                    <input id="album-image-url" class="rating-comment-submit" type="text" 
+                        placeholder="Image Link (URL)">
+                    <br>
+                    <br>
+                    <input id="record-label" class="rating-comment-submit" type="text" 
+                        placeholder="Record Label (optional)">
+                    <br>
+                    <br>
+                    <input id="submitAlbumButton" type="submit">
+                    
+                    
+                    <button type="button" onclick="hideSocials()">Dismiss</button>
+                </div>
+</div>
         </div>
 
     </div>
